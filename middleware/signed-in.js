d803b51fdae1ee08aec2d167a82748ee2e1c712d
gkourouma <gkourouma2@gmail.com>
2025-03-15 13:52:14 -0500

    updated sign-in wording

---
+++ middleware/signed-in.js
@@ -0,0 +1,6 @@
+const isSignedIn = (req, res, next) => {
+  if (req.session.user) return next(); //check if user is logged in and go to next middleware or route
+  res.redirect("/auth/sign-in");
+};
+
+module.exports = isSignedIn;
