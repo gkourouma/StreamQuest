f1da40e7aac74009fa893c70c2da51ef098ed895
gkourouma <gkourouma2@gmail.com>
2025-04-12 22:28:09 -0500

    added .currentUser to passUserToView

--- middleware/pass-user-to-view.js
+++ middleware/pass-user-to-view.js
@@ -1,8 +1,6 @@
 const passUserToView = (req, res, next) => {
-  res.locals.user = req.session.user ? req.session.user : null;
-  //anything we need to access in our templates globally can be added as a property to res.locals
-  //res is short for response
-  //generating templates is part of the response
+  res.locals.user = req.session.user || null;
+  res.locals.currentUser = req.session.user || null;
   next();
 };
 
