a032b611353300a42ab8ed29061ab49b18d71d57
gkourouma <gkourouma2@gmail.com>
2025-04-12 22:41:12 -0500

    map for available genres

---
+++ utils/genreMap.js
@@ -0,0 +1,13 @@
+module.exports = {
+    28: "Action",
+    35: "Comedy",
+    18: "Drama",
+    27: "Horror",
+    10749: "Romance",
+    878: "Sci-Fi",
+    16: "Animation",
+    12: "Adventure",
+    80: "Crime",
+    99: "Documentary"
+  };
+  
